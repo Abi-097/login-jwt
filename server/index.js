67b6dd6b950e881c51e0aff907b16ddcd5b303de
Abi-097 <abishekraja84@gmail.com>
2023-10-18 23:23:45 +0530

    basic ui created and add state

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
 
+// const express = require('express')
 const app = express()
 
-app.use(express.json())
 
-const MONGODB_SERVER = 
-mongoose.connect()
+app.get('/hello',(req, res)=>{
+res.send('hello world')
+})
+
+app.listen(3000, ()=>{
+    console.log("server is running on port 3000")
+})
+// app.use(express.json())
+// app.use(morgan('tiny'))
+
+// dotenv.config(server)
+// const MONGODB_SERVER = 
+// mongoose.connect()
