b6a15ba99632e1225efc51ff14342f6270f398dc
Abi-097 <abishekraja84@gmail.com>
2023-10-23 22:44:24 +0530

    .

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ res.send('hello world')
 app.listen(5000, ()=>{
     console.log("server is running on port 5000")
 })
-// app.use(express.json())
+// app.use(express.json())  
 // app.use(morgan('tiny'))
 
 // dotenv.config(server)
